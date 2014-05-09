2e447ef90f763baae940c057dc4f205cf1dc663d
Erick Birbe <erickcion@gmail.com>
2014-05-09 13:40:20 -0430

    Changing tabs for spaces

--- config.py
+++ config.py
@@ -5,16 +5,16 @@
 #
 
 COMPARE_REPOS = [
-#	(
-#		"http://security.debian.org/debian-security/ stable/updates main contrib non-free",
-#		"http://paquetes.canaima.softwarelibre.gob.ve/ estable main aportes no-libres",
-#		("i386","amd64")
-#	),
-	(
-		"http://repositorio.canaima.softwarelibre.gob.ve/ kerepakupai usuarios aportes no-libres",
-		"http://repositorio.canaima.softwarelibre.gob.ve/ kukenan usuarios aportes no-libres",
-		("i386","amd64")
-	),
+#    (
+#        "http://security.debian.org/debian-security/ stable/updates main contrib non-free",
+#        "http://paquetes.canaima.softwarelibre.gob.ve/ estable main aportes no-libres",
+#        ("i386","amd64")
+#    ),
+    (
+        "http://repositorio.canaima.softwarelibre.gob.ve/ kerepakupai usuarios aportes no-libres",
+        "http://repositorio.canaima.softwarelibre.gob.ve/ kukenan usuarios aportes no-libres",
+        ("i386","amd64")
+    ),
 
 
 ]
